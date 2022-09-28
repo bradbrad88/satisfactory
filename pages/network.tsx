b8d89ce9e4122989d259d76f9c8953ad1d38b297
Brad Teague <b_rad88@live.com>
2022-09-28 14:52:11 +0930

    Navigate off factory page if no active venture

--- pages/network.tsx
+++ pages/network.tsx
@@ -1,17 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Map from "components/features/Map";
 import NetworkMap from "components/features/NetworkMap";
 import "components/features/NetworkMap";
 import FactoryList from "components/features/FactoryList";
 import OverlayToolbar from "components/features/OverlayToolbar";
 import NetworkToolbar from "components/features/NetworkToolbar";
+import { useAppSelector } from "app/hooks";
+import { getActiveVenture } from "app/slices/ventures";
+import { useRouter } from "next/router";
 
 const panelOptions = {
   factories: "FACTORIES",
 };
 
 const Network = () => {
-  const [leftPanel, setLeftPanel] = useState("");
+  const [leftPanel, setLeftPanel] = useState(panelOptions.factories);
+  const activeVenture = useAppSelector(getActiveVenture);
+  const router = useRouter();
+  useEffect(() => {
+    if (!activeVenture) router.replace("/");
+  });
 
   const toggleFactories = () => {
     if (leftPanel === panelOptions.factories) return setLeftPanel("");
