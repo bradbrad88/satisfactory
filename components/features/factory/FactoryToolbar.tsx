318e4adcb9a08b275fda90e81334c14e7c3424a9
Brad Teague <b_rad88@live.com>
2022-09-28 22:36:59 +0930

    Rework factory toolbar to display item lookup list

--- components/features/factory/FactoryToolbar.tsx
+++ components/features/factory/FactoryToolbar.tsx
@@ -1,28 +1,21 @@
 import Link from "next/link";
-import { action } from "app/slices/entities";
-import { useAppDispatch, useAppSelector } from "app/hooks";
-import { getProductionSteps } from "app/slices/productionSteps";
 import Button from "components/common/Button";
 
 interface Proptypes {
-  toggleProductionSteps: () => void;
+  toggleItemPanel: () => void;
 }
 
-const FactoryToolbar = ({ toggleProductionSteps }: Proptypes) => {
-  const dispatch = useAppDispatch();
-  const buildingSteps = useAppSelector(getProductionSteps);
-  console.log(buildingSteps);
-
+const FactoryToolbar = ({ toggleItemPanel }: Proptypes) => {
   return (
-    <div className="flex h-full w-full text-white border-zinc-300 border-t-2 bg-zinc-900 p-5 font-bold text-xl gap-5">
+    <div className="flex h-full w-full text-white font-bold text-xl gap-5">
       <div className="flex h-12 w-full gap-3">
         <Link href={"/network"} passHref>
           <Button onClick={() => {}} colour="white" textColour="black">
             Network
           </Button>
         </Link>
-        <Button onClick={toggleProductionSteps} colour="white" textColour="black">
-          Production Steps
+        <Button onClick={toggleItemPanel} colour="white" textColour="black">
+          Produce New Item
         </Button>
         <Button onClick={() => {}} colour="white" textColour="black">
           Delete Factory
