ee83ce16b1837be4bef4a6274d9cbc63b009b4fc
Brad Teague <b_rad88@live.com>
2022-09-29 11:54:55 +0930

    Rearrange production step UI

--- components/features/factory/ProductionStep.tsx
+++ components/features/factory/ProductionStep.tsx
@@ -1,11 +1,12 @@
 import { useAppDispatch, useAppSelector } from "app/hooks";
 import { action } from "app/slices/entities";
-import { getProductionStep } from "app/slices/productionSteps";
+import { getByProducts, getProductionStep } from "app/slices/productionSteps";
 import DeleteButton from "components/common/DeleteButton";
-import { items } from "data";
+import { items, recipes, buildings } from "data";
 import QtySelector from "./QtySelector";
 import RecipeSelector from "./RecipeSelector";
 import RequiredInputs from "./RequiredInputs";
+import ByProduct from "./ByProduct";
 
 interface Proptypes {
   id: string;
@@ -13,23 +14,57 @@ interface Proptypes {
 
 const ProductionStep = ({ id }: Proptypes) => {
   const productionStep = useAppSelector(getProductionStep(id));
+  const byProducts = useAppSelector(getByProducts(id));
   const dispatch = useAppDispatch();
   const onDelete = () => {
     dispatch(action.destroyProductionStep(id));
   };
+
+  const renderByProducts = () => {
+    return (
+      byProducts?.map(byProduct => (
+        <ByProduct
+          key={productionStep.id + byProduct.item}
+          productionStep={productionStep.id}
+          product={byProduct}
+        />
+      )) || []
+    );
+  };
+
   return (
-    <div className="relative grid grid-flow-row min-w-[300px] border-zinc-300 border-2 rounded-lg overflow-hidden p-3 bg-zinc-700 text-white gap-3">
-      <div className="absolute right-1 top-1">
+    <div className="relative grid grid-flow-row min-w-[300px] border-zinc-300 border-2 rounded-lg overflow-hidde p-3 bg-zinc-700 text-white gap-3">
+      <div className="flex h-fit justify-between gap-3">
+        {/* Toolbar */}
+        <RecipeSelector
+          productionStep={productionStep.id}
+          item={productionStep.product.item}
+          selected={productionStep.recipe}
+        />
         <DeleteButton onDelete={onDelete} />
       </div>
-      <h1>{items.map[productionStep.product.item].name}</h1>
-      <QtySelector productionStep={productionStep} />
+      <div>
+        {/* Building & Power */}
+        <div>{buildings.map[recipes.map[productionStep.recipe].building].name}</div>
+      </div>
+      <div className="flex gap-3">
+        <div className="">
+          {/* Product */}
+          <h2>Product</h2>
+          <div className="min-w-[100px] border-2 border-zinc-300 rounded-md p-2">
+            <h2 className="font-bold">{items.map[productionStep.product.item].name}</h2>
+            <QtySelector productionStep={productionStep} />
+          </div>
+        </div>
+        <div className="">
+          {/* By Products */}
+          <h2>By Products</h2>
+          <div className="grid grid-flow-col">{renderByProducts()}</div>
+        </div>
+      </div>
+
       {/* Recipe Selector */}
-      <RecipeSelector
-        productionStep={productionStep.id}
-        item={productionStep.product.item}
-        selected={productionStep.recipe}
-      />
+
       {/* Render byProducts */}
 
       {/* Render  */}
