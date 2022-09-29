ac63fe21b143b70a069c2a796a9b43c989d5ac8e
Brad Teague <b_rad88@live.com>
2022-09-29 09:35:45 +0930

    Wire up destroy factory button in toolbar

--- components/features/factory/FactoryToolbar.tsx
+++ components/features/factory/FactoryToolbar.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
 import Button from "components/common/Button";
+import { useAppDispatch } from "app/hooks";
+import { action } from "app/slices/entities";
 
 interface Proptypes {
   toggleItemPanel: () => void;
 }
 
 const FactoryToolbar = ({ toggleItemPanel }: Proptypes) => {
+  const dispatch = useAppDispatch();
+  const destroyFactory = () => {
+    dispatch(action.destroyFactory());
+  };
+
   return (
     <div className="flex h-full w-full text-white font-bold text-xl gap-5">
       <div className="flex h-12 w-full gap-3">
@@ -17,7 +24,7 @@ const FactoryToolbar = ({ toggleItemPanel }: Proptypes) => {
         <Button onClick={toggleItemPanel} colour="white" textColour="black">
           Produce New Item
         </Button>
-        <Button onClick={() => {}} colour="white" textColour="black">
+        <Button onClick={destroyFactory} colour="white" textColour="black">
           Delete Factory
         </Button>
       </div>
