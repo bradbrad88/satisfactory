8b62ac23a147e333b1ed39ef7b9002df3478772d
Brad Teague <b_rad88@live.com>
2022-09-30 12:35:30 +0930

    Use iodrag with required inputs

--- components/features/factory/RequiredInputs.tsx
+++ components/features/factory/RequiredInputs.tsx
@@ -1,32 +1,22 @@
-import { ProductionStep } from "app/slices/productionSteps";
-import Container from "components/common/Container";
-import { items, recipes } from "data";
-import { Ingredient } from "data/recipes";
+import { useAppSelector } from "app/hooks";
+import { getRequiredInputs } from "app/slices/productionSteps";
+import IODrag from "./IODrag";
 interface Proptypes {
-  productionStep: ProductionStep;
+  productionStep: string;
 }
 
-const Ingredient = ({ ingredient, ratio }: { ingredient: Ingredient; ratio: number }) => {
-  return (
-    <Container>
-      <h2 className="font-bold"> {items.map[ingredient.item].name}</h2>
-      <div className="p-2">{(ingredient.amount * ratio).toFixed(4)}</div>
-    </Container>
-  );
-};
-
 const RequiredInputs = ({ productionStep }: Proptypes) => {
-  const recipe = recipes.map[productionStep.recipe];
-  const product = recipe.product.find(product => product.item === productionStep.product.item);
-  const ratio = productionStep.product.amount / (product?.amount || 1);
+  const ingredients = useAppSelector(getRequiredInputs(productionStep));
+
   const renderIngredients = () =>
-    recipe.ingredients.map(ingredient => (
-      <Ingredient
-        key={productionStep.id + ingredient.item}
-        ingredient={ingredient}
-        ratio={ratio}
+    ingredients?.map(ingredient => (
+      <IODrag
+        key={productionStep + ingredient.item + "input"}
+        product={ingredient}
+        io="input"
+        productionStep={productionStep}
       />
-    ));
+    )) || [];
   return <div className="grid auto-cols-min grid-flow-col gap-3">{renderIngredients()}</div>;
 };
 
