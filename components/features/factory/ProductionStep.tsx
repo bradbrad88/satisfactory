b49c8a2d6520fb9eb6add3dff46bcfdc61cafd4f
Brad Teague <b_rad88@live.com>
2022-10-03 20:12:51 +1030

    Wrap production step in positioner component

--- components/features/factory/ProductionStep.tsx
+++ components/features/factory/ProductionStep.tsx
@@ -7,9 +7,11 @@ import RecipeSelector from "./RecipeSelector";
 import RequiredInputs from "./RequiredInputs";
 import ProductionDetails from "./ProductionDetails";
 import Outputs from "./Outputs";
+import Positioner from "components/common/Positioner";
 
 interface Proptypes {
   id: string;
+  // scale: number;
 }
 
 const ProductionStep = ({ id }: Proptypes) => {
@@ -21,40 +23,47 @@ const ProductionStep = ({ id }: Proptypes) => {
   };
 
   return (
-    <div
-      className="relative grid grid-flow-row h-fit min-w-[300px] border-zinc-300 border-2 rounded-lg bg-zinc-700 text-white gap-3 p-3"
-      // onDrop={e => e.stopPropagation()}
-      onDragOver={e => e.stopPropagation()}
+    <Positioner
+      id={id}
+      updater={action.updateLocation}
+      x={productionStep.location.x}
+      y={productionStep.location.y}
     >
-      <div className="flex h-fit justify-between gap-5">
-        {/* Toolbar */}
-        <RecipeSelector
-          productionStep={productionStep.id}
-          item={productionStep.product.item}
-          selected={productionStep.recipe}
-        />
-        <h2 className="font-bold text-lg">{items.map[productionStep.product.item].name}</h2>
-        <DeleteButton onDelete={onDelete} />
-      </div>
+      <div
+        className="relative grid grid-flow-row h-fit min-w-[300px] border-zinc-300 border-2 rounded-lg bg-zinc-700 text-white gap-3 p-3 select-none"
+        // onDrop={e => e.stopPropagation()}
+        onDragOver={e => e.stopPropagation()}
+      >
+        <div className="flex h-fit justify-between gap-5">
+          {/* Toolbar */}
+          <RecipeSelector
+            productionStep={productionStep.id}
+            item={productionStep.product.item}
+            selected={productionStep.recipe}
+          />
+          <h2 className="font-bold text-lg">{items.map[productionStep.product.item].name}</h2>
+          <DeleteButton onDelete={onDelete} />
+        </div>
 
-      {/* Production Details and Input/Output */}
-      <div className="flex gap-3">
-        {/* Production Details */}
-        <ProductionDetails productionStep={id} />
+        {/* Production Details and Input/Output */}
+        <div className="flex gap-3">
+          {/* Production Details */}
+          <ProductionDetails productionStep={id} />
 
-        {/* Inputs / Outputs */}
-        <div className="flex flex-col gap-3">
-          {/* Outputs */}
-          <Outputs productionStepId={id} />
+          {/* Inputs / Outputs */}
+          <div className="flex flex-col gap-3">
+            {/* Outputs */}
+            <Outputs productionStepId={id} />
 
-          {/* Inputs */}
-          <div className="bg-zinc-800 p-2 rounded-md">
-            <h2 className="text-amber-500">Inputs</h2>
-            <RequiredInputs productionStep={id} />
+            {/* Inputs */}
+            <div className="bg-zinc-800 p-2 rounded-md">
+              <h2 className="text-amber-500">Inputs</h2>
+              <RequiredInputs productionStep={id} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Positioner>
   );
 };
 
