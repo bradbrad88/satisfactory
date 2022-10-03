62108d8b1a520f4d4ee9aa5c838771bc46460be8
Brad Teague <b_rad88@live.com>
2022-10-03 19:56:50 +1030

    Map details on UI slice

--- app/slices/ui.ts
+++ app/slices/ui.ts
@@ -11,6 +11,8 @@ interface Drag {
 const initialState = {
   drag: null as Drag | null,
   loaded: false,
+  mapScale: 1,
+  mapBoundingRect: { left: 0, top: 0, height: 0, width: 0 },
 };
 
 const slice = createSlice({
@@ -23,13 +25,29 @@ const slice = createSlice({
     setLoaded: (state: UI) => {
       state.loaded = true;
     },
+    setMapScale: (state: UI, action: { payload: number }) => {
+      state.mapScale = action.payload;
+    },
+    setMapBoundingRect: (
+      state: UI,
+      action: { payload: { left: number; top: number; height: number; width: number } }
+    ) => {
+      console.log("Setting bounding rect in redux");
+      state.mapBoundingRect = action.payload;
+    },
   },
 });
 
-export const { dragElement, setLoaded } = slice.actions;
+export const { dragElement, setLoaded, setMapScale, setMapBoundingRect } = slice.actions;
 export default slice.reducer;
 export type UI = typeof initialState;
 
 export const getDragElement = (state: RootState) => {
   return state.ui.drag;
+};
+
+export const getMapDetails = (state: RootState) => {
+  const mapScale = state.ui.mapScale;
+  const mapBoundingRect = state.ui.mapBoundingRect || {};
+  return { ...mapBoundingRect, mapScale };
 };
